2abc1bd0263861338e704e76ff4bddfdb8003216
suren-11 <presanth11@gmail.com>
2023-08-28 20:17:54 +0530

    created customerDao, itemDao

---
+++ Thogakade-master/src/com/seekerscloud/pos/dao/custom/ItemDao.java
@@ -0,0 +1,11 @@
+package com.seekerscloud.pos.dao.custom;
+
+import com.seekerscloud.pos.dao.CrudDao;
+import com.seekerscloud.pos.entity.Item;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public interface ItemDao extends CrudDao<Item, String> {
+
+}
