03b54f44b9df78a9aaab5bf5ae538eeaa2f775a4
suren-11 <presanth11@gmail.com>
2023-08-28 21:18:52 +0530

    Add CrudUtil codes to CustomerDaoImpl, ItemDaoImpl

--- Thogakade-master/src/com/seekerscloud/pos/dao/CrudUtil.java
+++ Thogakade-master/src/com/seekerscloud/pos/dao/CrudUtil.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CrudUtil {
+   /* ============== Legacy way ===================
    private static PreparedStatement getPreparedStatement(String sql,Object...params) throws SQLException, ClassNotFoundException {
        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement(sql);
        for (int i =0; i < params.length; i++){
@@ -14,22 +15,25 @@ public class CrudUtil {
        }
        return statement;
    }
-    /*public static ResultSet executeQuery(String sql, Object...params) throws SQLException, ClassNotFoundException {
-        return getPreparedStatement(sql, params).executeQuery();
-    }
-    public static boolean executeUpdate(String sql, Object...params) throws SQLException, ClassNotFoundException {
-        return getPreparedStatement(sql, params).executeUpdate()>0;
-    }*/
 
-   /*public static <T> T execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
+   public static ResultSet executeQuery(String sql, Object...params) throws SQLException, ClassNotFoundException {
+       return getPreparedStatement(sql, params).executeQuery();
+   }
+   public static boolean executeUpdate(String sql, Object...params) throws SQLException, ClassNotFoundException {
+       return getPreparedStatement(sql, params).executeUpdate()>0;
+   }*/
+
+    //============Fancy Way==========================================
+   public static <T> T execute(String sql,Object...params) throws SQLException, ClassNotFoundException {
        PreparedStatement statement = DBConnection.getInstance().getConnection().prepareStatement(sql);
-       for (int i = 0; i < params.length; i++) {
+       for (int i =0; i < params.length; i++){
            statement.setObject((i+1),params[i]);
        }
        if (sql.startsWith("SELECT")){
-           return (T)statement.executeQuery();
+           return (T) statement.executeQuery();
        }
-       return (T)(Boolean)(statement.executeUpdate()>0);
-   }*/
+       return (T) (Boolean)(statement.executeUpdate() > 0);
+   }
+
 
 }
